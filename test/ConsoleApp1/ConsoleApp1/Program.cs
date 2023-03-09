71b1b4ea2f1f23c8c2ccd45c388231800fbd342b
Asus <Asus@DESKTOP-IJEPJQM>
2023-03-09 05:05:42 -0800

    line between of sentences

---
+++ test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sifarisin sayi : ");
+            int carOrderCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Marka : ");
+            string carBrandName = Console.ReadLine();
+            Console.WriteLine("Model : ");
+            string carModelName = Console.ReadLine();
+            Console.WriteLine("Buraxilis ili : ");
+            int carYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Negd qiymeti : ");
+            decimal carCashPrice = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("##################################");
+            Console.WriteLine("");
+
+            Console.WriteLine("Kredit faizi : ");
+            decimal carCreditPercent = decimal.Parse(Console.ReadLine());
+            int creditTimeRules = 3;
+            int PercentForEveryYear = 2;
+            Console.WriteLine("Kredit muddeti : ");
+            decimal carCreditTime = decimal.Parse(Console.ReadLine());
+            decimal carCreditPrice = 0;
+
+            if (carCreditTime >= creditTimeRules)
+            {
+                carCreditPrice = carCashPrice  + carCashPrice * ((carCreditPercent) + (carCreditTime - creditTimeRules) * PercentForEveryYear) / (100);
+                Console.WriteLine("Kredit qiymeti : " + carCreditPrice);
+            }
+            else
+            {
+                carCreditPrice = carCashPrice  + carCashPrice * carCreditPercent / 100;
+                Console.WriteLine("Kredit qiymeti : " + carCreditPrice);
+            }
+
+
+
+
+
+        }
+    }
+}
